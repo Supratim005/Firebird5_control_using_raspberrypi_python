b6e6b46326411d1fc874b2b736e45a9b3be27294
Supratim Dey <supratimdey5@gmail.com>
2022-05-26 11:21:42 +0530

    -

---
+++ project/python3.7/calibration.py
@@ -0,0 +1,22 @@
+import sys, select, os
+import gps
+import casadi as ca
+import heading
+
+
+def calibaration():
+	while True:
+		os.system('cls' if os.name == 'nt' else 'clear')
+		x,y,_ = gps.position()
+		h = heading.heading()
+		init_state = ca.vertcat(
+		ca.horzcat(x), # East
+		ca.horzcat(y), # North
+		ca.horzcat(h)  # heading angle
+		)
+		print("I'm calibarating. Press Enter to stop!")
+		print("X:",x,"Y:",y,"Heading:",h)
+		if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+			line = raw_input()
+			return init_state 
+			break
