eacdb9f1459dbeb22a405696c4673b4a8ff0334f
Supratim Dey <supratimdey5@gmail.com>
2022-05-30 03:49:49 +0530

    -

--- project/python3.7/test_heading.py
+++ project/python3.7/test_heading.py
@@ -1,8 +1,18 @@
 import calibration
 import heading
-import time
-state_init = calibration.calibration()        # initial state
 
+import time
+
+state_init = calibration.calibration()        # initial state
+#print(state_init[2])
+import board
+import adafruit_bno055
+i2c = board.I2C()
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+x=1
 while True:
-	heading.heading()
+	y=heading.heading(sensor)
+	y=state_init[2]-y
+	print(y*(1260/22))
 	time.sleep(1)
